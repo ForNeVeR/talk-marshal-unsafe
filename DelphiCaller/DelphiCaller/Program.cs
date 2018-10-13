84145b80578989562548271d90917e29cad7b5cd
Friedrich von Never <friedrich@fornever.me>
2018-10-13 20:34:08 +0700

    Add Delphi prototype

---
+++ DelphiCaller/DelphiCaller/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DelphiCaller
+{
+    class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate int MyCall(int x);
+
+        [DllImport("DelphiLib.dll", CallingConvention = CallingConvention.StdCall)]
+        extern static int DoCall(MyCall callback);
+        
+        static void Main()
+        {
+            Console.WriteLine("Test program");
+            var x = DoCall(val =>
+            {
+                Console.WriteLine("Inside lambda:");
+                Console.WriteLine(val);
+                return val * 2;
+            });
+            Console.WriteLine("Outside lambda: " + x);
+        }
+    }
+}
---
+++ DelphiLib/DelphiLib.dpr
@@ -0,0 +1,20 @@
+library DelphiLib;
+
+uses
+  System.SysUtils,
+  System.Classes;
+
+{$R *.res}
+
+type
+ TMyCallback = function(number: Integer): Integer;
+
+function DoCall(callback: TMyCallback): Integer; stdcall;
+begin
+  Result := callback(10);
+end;
+
+exports
+  DoCall;
+
+end.
